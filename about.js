d02c8c2a9d4e09acdcebc29e6b4a0e470e93f2e7
AdamDang1 <dtdang1@gmail.com>
2022-03-26 14:12:21 -0500

    finished parts 1 & 2 of unit 3 assessment

--- about.js
+++ about.js
@@ -5,10 +5,16 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('Form has been submitted successfully!');
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+let happyDuck = document.querySelector('#duckPic');
+
+happyDuck.addEventListener('mouseover', () => {
+	alert('Look at you, crushing this assessment!')
+});
